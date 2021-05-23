a13940d4edee3a601e85d594dee26537f9c5b06c
YongHun Lee <yosiyong@gmail.com>
2021-05-23 21:21:51 +0900

    再レンダリング memo,useCallback,useMemo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useState, useCallback, useMemo } from "react";
+import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
 export default function App() {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
